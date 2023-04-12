4eabf3b008a422f199887e7bf014400ae5c4216b
“yufengjie” <2457522528@qq.com>
2023-04-12 14:31:08 +0800

    颜色渐变对起始终止颜色加时间三角函数

--- src/views/shaderColor/fragment1.glsl
+++ src/views/shaderColor/fragment1.glsl
@@ -8,13 +8,21 @@ uniform vec2 u_resolution;
 uniform vec2 u_mouse;
 uniform float u_time;
 
-vec3 colorA = vec3(0.149,0.141,0.912);
-vec3 colorB = vec3(1.000,0.833,0.224);
+// vec3 colorA = vec3(0.912,0.894,0.000);
+// vec3 colorB = vec3(0.025,0.006,1.000);
 
 void main() {
-	float x = gl_FragCoord.x / u_resolution.x;
-	float a = sin(x + sin(u_time));
 
-	vec3 color = mix(colorA, colorB, a);
+	vec3 colorA = vec3(cos(u_time));
+	vec3 colorB = vec3(sin(u_time));
+
+	vec2 st = gl_FragCoord.xy / u_resolution.xy;
+	vec3 pct = vec3(1.0);
+
+	pct.r = smoothstep(0.0, 1.0, st.x);
+	pct.g = sin(pct.x * PI);
+	pct.b = pow(st.x,0.5);
+
+	vec3 color = mix(colorA, colorB, pct);
 	gl_FragColor = vec4(color, 1.0);
 }
