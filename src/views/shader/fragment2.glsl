c589987194697a0b1a0fd624b71f5c01efab8519
“yufengjie” <2457522528@qq.com>
2023-06-13 16:29:32 +0800

    feat: f

--- src/views/shader/fragment2.glsl
+++ src/views/shader/fragment2.glsl
@@ -2,10 +2,16 @@ uniform vec2 u_resolution;
 uniform float u_time;
 
 void main() {
-  vec2 st = gl_FragCoord.xy / u_resolution.xx;
-  float pix = 5.0 / u_resolution.x;
+  vec2 st = gl_FragCoord.xy / u_resolution.xy;
+  // float pix = 5.0 / u_resolution.x;
 
-  float l = smoothstep(0.0, pix, st.x - st.y);
+  vec2 pos = vec2(0.5, 0.5) - st;
+  float r = length(pos) * 3.0;
+  float a = atan(pos.y, pos.x);
 
-  gl_FragColor = vec4(l);
+  float f = abs(cos(a * 20.0) * sin(a * abs(sin(u_time) * 10.0))) * .8 + .1;
+
+  vec3 color = vec3(1.0-smoothstep(f, f + 0.02, r), 0.4, 0.4);
+
+  gl_FragColor = vec4(color, 1.0);
 }
