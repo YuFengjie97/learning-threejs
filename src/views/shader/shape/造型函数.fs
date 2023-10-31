7956cd21e7124c9d4cc8ec745e7ced93f5eb8d45
yufengjie <2457522528@qq.com>
2023-10-31 15:40:23 +0800

    造型函数

--- src/views/shader/shape/造型函数.fs
+++ src/views/shader/shape/造型函数.fs
@@ -24,18 +24,32 @@ float random(float x) {
   return fract(sin(x));
 }
 
+vec3 rgb2hsb(in vec3 c) {
+  vec4 K = vec4(0.0, -1.0 / 3.0, 2.0 / 3.0, -1.0);
+  vec4 p = mix(vec4(c.bg, K.wz), vec4(c.gb, K.xy), step(c.b, c.g));
+  vec4 q = mix(vec4(p.xyw, c.r), vec4(c.r, p.yzx), step(p.x, c.r));
+  float d = q.x - min(q.w, q.y);
+  float e = 1.0e-10;
+  return vec3(abs(q.z + (q.w - q.y) / (6.0 * d + e)), d / (q.x + e), q.x);
+}
+
+vec3 hsb2rgb(in vec3 c) {
+  vec3 rgb = clamp(abs(mod(c.x * 6.0 + vec3(0.0, 4.0, 2.0), 6.0) - 3.0) - 1.0, 0.0, 1.0);
+  rgb = rgb * rgb * (3.0 - 2.0 * rgb);
+  return c.z * mix(vec3(1.0), rgb, c.y);
+}
+
 void main() {
   vec2 st = gl_FragCoord.xy / u_resolution;
   vec3 color = vec3(0.0);
 
-  float tpct = 10.0;
-
-  for(float k=-4.0;k<=4.0;k+=0.1) {
+  for(float k = -4.0; k <= 4.0; k += 0.1) {
     float pct = gain(st.x, k);
     float cpct = plot(st, pct);
-    vec3 cc = cpct * vec3(random(u_time/pct + k),random(k), random(u_time/tpct + k));
+    vec3 hsl = vec3(sin(u_time + k),1.0,0.5);
+    vec3 rgb = hsb2rgb(hsl);
 
-    color += cc;
+    color += cpct * rgb;
   }
 
   gl_FragColor = vec4(color, 1.0);
