f582870c9de62ef7b2f18177d0b6f4b037e84229
yufengjie <2457522528@qq.com>
2023-11-07 15:35:59 +0800

    f

--- src/views/shader/shape/polar_shape_2.fs
+++ src/views/shader/shape/polar_shape_2.fs
@@ -15,19 +15,6 @@ float easeOutQuart(float x) {
   return 1. - pow(1. - x, 4.);
 }
 
-float polar(vec2 st, float gap) {
-  float res = 0.0;
-  // 同心圆
-  for(float r = 0.0; r < 2.0; r += gap) {
-    res += 1. - SMOOTH(0., abs(st.x - r), 4.);
-  }
-  //直线
-  for(float a = 0.; a <= 2.0; a += .25) {
-    res += 1. - SMOOTH(0., abs(PI * a - st.y), 10.);
-  }
-  return res;
-}
-
 vec2 toPolar(vec2 st) {
   float r = length(st);
   float theta = mod(atan(st.y, st.x), 2. * PI);
@@ -55,10 +42,6 @@ void main() {
   st = toPolar(st);
 
   vec3 c_fin = vec3(0.0);
-
-  // 显示坐标系
-  // float pol = polar(st, 0.1);
-  // c_fin += c_white * pol;
 
   st.x *= 8.;
 
