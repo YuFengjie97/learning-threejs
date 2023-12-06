07e0611519142f36c66b5e9dabad7b05a442a088
yufengjie <2457522528@qq.com>
2023-12-06 18:29:27 +0800

    f

--- src/views/shader/shape/noise.fs
+++ src/views/shader/shape/noise.fs
@@ -21,6 +21,30 @@ float sdBox(in vec2 p, in vec2 b) {
   return length(max(d, 0.0)) + min(max(d.x, d.y), 0.0);
 }
 
+float ndot(vec2 a, vec2 b ) { return a.x*b.x - a.y*b.y; }
+float sdRhombus( in vec2 p, in vec2 b ) 
+{
+    p = abs(p);
+    float h = clamp( ndot(b-2.0*p,b)/dot(b,b), -1.0, 1.0 );
+    float d = length( p-0.5*b*vec2(1.0-h,1.0+h) );
+    return d * sign( p.x*b.y + p.y*b.x - b.x*b.y );
+}
+
+
+float sdStar5(in vec2 p, in float r, in float rf)
+{
+    const vec2 k1 = vec2(0.809016994375, -0.587785252292);
+    const vec2 k2 = vec2(-k1.x,k1.y);
+    p.x = abs(p.x);
+    p -= 2.0*max(dot(k1,p),0.0)*k1;
+    p -= 2.0*max(dot(k2,p),0.0)*k2;
+    p.x = abs(p.x);
+    p.y -= r;
+    vec2 ba = rf*vec2(-k1.y,k1.x) - vec2(0,1);
+    float h = clamp( dot(p,ba)/dot(ba,ba), 0.0, r );
+    return length(p-ba*h) * sign(p.y*ba.x-p.x*ba.y);
+}
+
 vec3 palette(float t) {
   vec3 a = vec3(1.000,0.500,0.500);
   vec3 b = vec3(0.500,0.500,0.500);
@@ -42,15 +66,17 @@ vec3 createBox(vec2 st, float seed) {
 
   float theta = sin(noise(iTime+i)) * PI*4.;
   st = rotate(st, theta);
-  float size = abs(sin(noise(iTime+i)));
+  float v1 = abs(sin(noise(iTime+i)));
+  float v2 = abs(sin(noise(iTime+i+10.)));
+  float v3 = abs(sin(noise(iTime+i+20.)));
   float xoff = (sin(noise(iTime+i)))/10.;
   float yoff = (sin(noise(iTime+i)))/10.;
   st += vec2(xoff,yoff);
   vec3 color = palette(noise(iTime*i));
 
-  float box = sdBox(st,vec2(size,size));
-  float smoothStart = abs(sin(noise(iTime + i)))*10.*pix;
-  float smoothEnd = abs(sin(noise(iTime + i)))*40.*pix;
+  float box = sdStar5(st,v1,v2);
+  float smoothStart = abs(sin(noise(iTime * i)))*10.*pix;
+  float smoothEnd = abs(sin(noise(iTime * i)))*40.*pix;
   float b = 1. - smoothstep(smoothStart,smoothEnd,abs(box-0.1));
 
   return b*color;
