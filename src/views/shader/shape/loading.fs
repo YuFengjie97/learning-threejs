2ca9254c9d007981a43054c0244d5a411bed7053
yufengjie <2457522528@qq.com>
2023-12-09 19:01:26 +0800

    d

--- src/views/shader/shape/loading.fs
+++ src/views/shader/shape/loading.fs
@@ -18,15 +18,6 @@ float easeOutElastic(float x) {
   return x == 0. ? 0. : x == 1. ? 1. : pow(2., -10. * x) * sin((x * 10. - 0.75) * c4) + 1.;
 }
 
-vec3 palette(float t) {
-  vec3 a = vec3(1.000,0.500,0.500);
-  vec3 b = vec3(0.500,0.500,0.500);
-  vec3 c = vec3(0.750,1.000,0.667);
-  vec3 d = vec3(0.800,1.000,0.333);
-  return a + b * cos(6.28318 * (c * t + d));
-}
-
-
 void main() {
   pix = 1. / iResolution.y;
   vec2 st = (gl_FragCoord.xy * 2. - iResolution.xy) / min(iResolution.x, iResolution.y);
@@ -36,11 +27,8 @@ void main() {
 
   vec3 color_ring = vec3(0.13, 0.27, 0.28);
   vec3 color_act = vec3(0.0, 0.96, 0.83);
-  // vec3 color_ring = palette(t*0.2);
-  // vec3 color_act = palette(t*0.2+1.);
 
-
-  st = rotate(st, easeOutElastic(mod(t, 1.)));
+  st = rotate(st, PI/4.*easeOutElastic(mod(t, 1.)));
   float d = length(st);
   float theta = mod(atan(st.y, st.x), PI * 2.);
 
@@ -52,7 +40,9 @@ void main() {
     gap += 1. - smoothstep(90. * pix, 100. * pix, abs(mod(theta, PI / 2.) - a));
   }
 
-  float actIndex = 7.-mod(floor(t*30.), 8.);
+  float actIndex = 7.-mod(floor(t*4.), 8.);
+  //float actIndex = 7.-mod(floor(t), 8.);
+  
   float act = (step(0., theta - actIndex * PI / 4.) - step(0., theta - (actIndex + 1.) * PI / 4.));
 
   float ring1 = ring - gap;
